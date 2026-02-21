92cd64985b2b7e2634408e0b908ff2f09ee5eab3
David Wahlandt <david.wahlandt@rewe-digital.com>
2026-02-21 12:10:37 +0100

    Add support for tab content builder api in `CoordinatedTabView`

--- Sources/WarlyNavigationUI/CoordinatedTabView.swift
+++ Sources/WarlyNavigationUI/CoordinatedTabView.swift
@@ -5,30 +5,80 @@ import WarlyNavigation
 public struct CoordinatedTabView<C: View, T: TabItem>: View {
     @Environment(\.coordinatorStack) private var coordinatorStack
     @State private var manager: CoordinatedTabManager<T>
+    private let tabs: [T]
+    private var builder: ([T], _ selectedTab: Binding<TabID>) -> C
 
-    private let content: (T, _ isSelected: Bool) -> C
+    public var body: some View {
+        builder(tabs, $manager.selectedTab)
+            .onAppear {
+                manager.setUp(coordinatorStack: coordinatorStack)
+            }
+    }
+}
 
+extension CoordinatedTabView {
     /// Create a new coordinated tab view
     /// - Parameter tabs: The tabs to display in the tab bar
     /// - Parameter resolver: The navigation resolver to tregister the `TabDestination` with
-    public init(
+    /// - Parameter content: The ``Tab`` content.
+    init<TC: View>(
         tabs: [T],
         resolver: any NavigationResolver,
-        @ViewBuilder content: @escaping (T, _ isSelected: Bool) -> C
-    ) {
-        self.content = content
+        @ViewBuilder content: @escaping (T, _ isSelected: Bool) -> TC
+    ) where C == TabView<TabID, ForEach<[T], T.ID, TC>> {
+        self.tabs = tabs
         _manager = .init(initialValue: CoordinatedTabManager(resolver: resolver, tabs: tabs))
+        builder = { tabs, selectedTab in
+            TabView(selection: selectedTab) {
+                ForEach(tabs) { tab in
+                    content(tab, selectedTab.wrappedValue == tab.id)
+                }
+            }
+        }
+    }
+}
+
+@available(iOS 18.0, *)
+extension CoordinatedTabView {
+    /// Create a new coordinated tab view
+    /// - Parameter tabs: The tabs to display in the tab bar
+    /// - Parameter resolver: The navigation resolver to tregister the `TabDestination` with
+    /// - Parameter content: The ``Tab`` content.
+    public init<TC: TabContent<TabID>>(
+        tabs: [T],
+        resolver: any NavigationResolver,
+        @TabContentBuilder<TabID> content: @escaping ([T], _ isSelected: (T) -> Bool) -> TC
+    ) where C == TabContentView<T, TC> {
+        self.tabs = tabs
+        _manager = .init(initialValue: CoordinatedTabManager(resolver: resolver, tabs: tabs))
+        builder = { tabs, selectedTab in
+            TabContentView(selectedTab: selectedTab, tabs: tabs, isSelected: { selectedTab.wrappedValue == $0.id }, content: content)
+        }
+    }
+}
+
+@available(iOS 18.0, *)
+public struct TabContentView<T: TabItem, TC: TabContent<TabID>>: View {
+    @Binding var selectedTab: TabID
+    let tabs: [T]
+    let isSelected: (T) -> Bool
+    let content: ([T], _ isSelected: (T) -> Bool) -> TC
+
+    fileprivate init(
+        selectedTab: Binding<TabID>,
+        tabs: [T],
+        isSelected: @escaping (T) -> Bool,
+        content: @escaping ([T], _ isSelected: (T) -> Bool) -> TC
+    ) {
+        _selectedTab = selectedTab
+        self.tabs = tabs
+        self.isSelected = isSelected
+        self.content = content
     }
 
     public var body: some View {
-        TabView(selection: $manager.selectedTab) {
-            ForEach(manager.tabs) { tab in
-                content(tab, manager.selectedTab == tab.id)
-                    .onAppear(perform: manager.tabDidAppear)
-            }
-        }
-        .onAppear {
-            manager.setUp(coordinatorStack: coordinatorStack)
+        TabView(selection: $selectedTab) {
+            content(tabs, isSelected)
         }
     }
 }
@@ -38,7 +88,11 @@ public struct CoordinatedTabView<C: View, T: TabItem>: View {
 @Observable @MainActor
 private final class CoordinatedTabManager<T: TabItem> {
     let tabs: [T]
-    var selectedTab: TabID
+    var selectedTab: TabID {
+        didSet {
+            tabDidAppear()
+        }
+    }
 
     private let resolver: any NavigationResolver
     @ObservationIgnored private var didSetUp = false
@@ -78,7 +132,7 @@ private final class CoordinatedTabManager<T: TabItem> {
         }
     }
 
-    func tabDidAppear() {
+    private func tabDidAppear() {
         appearingContinuation?.resume()
         appearingContinuation = nil
     }
